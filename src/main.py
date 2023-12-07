d91931f361d6e95688142fb8c30f6ba57e1e30a6
FlipperEugene <bwatford077@gmail.com>
2023-12-07 15:14:47 -0500

    code

--- src/main.py
+++ src/main.py
@@ -14,8 +14,8 @@ controller_1 = Controller(PRIMARY)
 motor_18 = Motor(Ports.PORT18, GearSetting.RATIO_6_1, False)
 motor_19 = Motor(Ports.PORT19, GearSetting.RATIO_6_1, True)
 motor_20 = Motor(Ports.PORT20, GearSetting.RATIO_6_1, True)
-rightside = MotorGroup(motor_11, motor_12, motor_13)
-leftside = MotorGroup(motor_18, motor_19, motor_20)
+right_side = MotorGroup(motor_11, motor_12, motor_13)
+left_side = MotorGroup(motor_18, motor_19, motor_20)
 compMode = True
 #nerd
 """Extra Motors"""
@@ -23,28 +23,25 @@ motor_15 = Motor(Ports.PORT5, GearSetting.RATIO_6_1, False)
 motor_16 = Motor(Ports.PORT6, GearSetting.RATIO_6_1, True)
 intake = MotorGroup(motor_15, motor_16)
 wings = DigitalOut(brain.three_wire_port.a)
-climbmotor = Motor(Ports.PORT17, GearSetting.RATIO_36_1, False)
+climb_motor = Motor(Ports.PORT17, GearSetting.RATIO_36_1, False)
 gyro = Inertial(Ports.PORT8)
 
 """init"""
 intake.set_velocity(100, PERCENT)
-rightside.set_velocity(100, PERCENT)
-leftside.set_velocity(100, PERCENT)
-climbmotor.set_velocity(100, PERCENT)
-climbmotor.set_stopping(BRAKE)
+right_side.set_velocity(100, PERCENT)
+left_side.set_velocity(100, PERCENT)
+climb_motor.set_velocity(100, PERCENT)
+climb_motor.set_stopping(HOLD)
 
 """Wheel Base"""
 wheelDiameterCM = 8.255
 wheelTravel = 275
 trackWidth = 370
 wheelBase = 330
-gearRatio = 1
-drivetrain = SmartDrive(leftside, rightside, gyro, wheelTravel, trackWidth, wheelBase, MM, gearRatio)
+gearRatio = 1.667
+drivetrain = SmartDrive(left_side, right_side, gyro, wheelTravel, trackWidth, wheelBase, MM, gearRatio)
 
-class Team:
-    blue = 0
-    red = 1
-    skills = 2
+
 
 # Begin project code
 # Create callback functions for each controller button event
@@ -52,37 +49,47 @@ class Team:
 
 def onevent_controller_1buttonL1_pressed_0():
    
-    climbmotor.spin(FORWARD)
+    climb_motor.spin(FORWARD)
 
 def onevent_controller_1buttonL1_released_0():
    
-    climbmotor.stop()
+    climb_motor.stop()
 
 def onevent_controller_1buttonL2_pressed_0():
    
-    climbmotor.spin(REVERSE)
+    climb_motor.spin(REVERSE)
 
 def onevent_controller_1buttonL2_released_0():
    
-    climbmotor.stop()
+    climb_motor.stop()
 
 
 def forward(distance, speed):
+    # brain.screen.print("forward Start" + str(brain.timer.time(SECONDS)))
+    # brain.screen.new_line
     drivetrain.set_stopping(BRAKE)
     drivetrain.set_drive_velocity(speed, PERCENT)
     drivetrain.set_turn_velocity(speed, PERCENT)
     drivetrain.drive_for(FORWARD, distance, MM)
     drivetrain.stop()
+    # brain.screen.print("forward End" + str(brain.timer.time(SECONDS)))
+    # brain.screen.new_line()
     # drivetrain.set_stopping(COAST)
 
 def turnToHeading(heading, speed):
+    # brain.screen.print("turn start" + str(brain.timer.time(SECONDS)))
+    # brain.screen.new_line()
     drivetrain.set_stopping(BRAKE)
     drivetrain.set_drive_velocity(speed, PERCENT)
     drivetrain.set_turn_velocity(speed, PERCENT)
+    # brain.screen.new_line()
+    # brain.screen.print("Velocity done"+ str(brain.timer.time(SECONDS)))
+    # brain.screen.new_line()
      # Adjust the wait time as needed
 
-    drivetrain.turn_to_heading(heading)
+    drivetrain.turn_to_heading(heading, DEGREES)
     drivetrain.stop()
+    # brain.screen.print("turn stop" + str(brain.timer.time(SECONDS)))
     # drivetrain.set_stopping(COAST)
 
 
@@ -107,6 +114,9 @@ def pre_autonomous():
     mode = 3
     brain.screen.draw_image_from_file("boot.png", 0, 0)
     controller_1.screen.print("Carter Mode?")
+    global team
+    team = 1
+    """Team 2 = Red, Team 3 = Blue, Team 4 = Skills"""
     while mode not in (1, 2):
         if controller_1.buttonA.pressing():
             mode = 1
@@ -118,23 +128,57 @@ def pre_autonomous():
             controller_1.screen.clear_line(1)
             controller_1.screen.set_cursor(1,1)
             controller_1.screen.print("Good choice")
+    wait(2, SECONDS)
+    
+
+    while team not in (2, 3, 4):
+        if controller_1.buttonA.pressing():
+            team = 2
+            controller_1.screen.clear_line(1)
+            controller_1.screen.set_cursor(1,1)
+            controller_1.screen.print("Red Team")
+
+        if controller_1.buttonB.pressing():
+            team = 3
+            controller_1.screen.clear_line(1)
+            controller_1.screen.set_cursor(1,1)
+            controller_1.screen.print("Blue Team")
+
+        if controller_1.buttonX.pressing():
+            team = 3
+            controller_1.screen.clear_line(1)
+            controller_1.screen.set_cursor(1,1)
+            controller_1.screen.print("Skills")
+
+        
+
     if not brain.sdcard.is_inserted():
         brain.screen.print("No Sd card not logging")
     # autonomous()
 
 
 def autonomous():
-    forward(-700, 50)
-    turnToHeading(90, 10)
-    forward(-100, 10)
+    # gyro.calibrate()
+    # pass
+    # if mode == 1:
+    #     forward(2000, 100)
+    #     climb_motor.spin_for(FORWARD, 43, DEGREES)
+    #     turnToHeading(50, 50)
+    #     wings.set(True)
+    #     intake.spin(FORWARD)
+    #     forward(1450, 200)
+    #     turnToHeading(0, 50)
+    #     forward(850, 200)
+    #     turnToHeading(-90, 80)
+    #     wings.set(False)
+    #     forward(-600, 200)
+    #     forward(2200, 200)
+    #     forward(-500, 200)
+    #     intake.stop()
+    #     brain.screen.print("AUTO END AT" + str(brain.timer.time(SECONDS)))
     
-    climbmotor.spin_for(FORWARD, 436, DEGREES)
-    forward(500, 100)
-    # brain.screen.clear_screen()
-    brain.screen.print("autonomous code")
-    # place automonous code here
-
-
+    
+        
 
 def user_control():
     # brain.screen.clear_screen()
@@ -157,80 +201,32 @@ def user_control():
             intake.spin(FORWARD)
         else:
             intake.stop()
-
-        brain.screen.print("penis")
+        if controller_1.buttonB.pressing():
+            left_side.set_stopping(BRAKE)
+            right_side.set_stopping(BRAKE)
+        else:
+            left_side.set_stopping(COAST)
+            right_side.set_stopping(COAST)
         brain.screen.new_line()
 
         
         if mode == 2:
             """Tank steering"""
-            rightside.spin(FORWARD)
-            leftside.spin(FORWARD)
-            rightside.set_velocity(controller_1.axis2.position(), PERCENT)
-            leftside.set_velocity(controller_1.axis3.position(), PERCENT)
+            right_side.spin(FORWARD)
+            left_side.spin(FORWARD)
+            right_side.set_velocity(controller_1.axis2.position(), PERCENT)
+            left_side.set_velocity(controller_1.axis3.position(), PERCENT)
         elif mode == 1:
             """Single stick drive"""
-            rightside.spin(FORWARD)
-            leftside.spin(FORWARD)
-            rightside.set_velocity((controller_1.axis3.position() - controller_1.axis4.position()), PERCENT)
-            leftside.set_velocity((controller_1.axis3.position() + controller_1.axis4.position()), PERCENT)
-
-# class Logging(object):
-    """
-    A class that can run multiple logs for different events and store their outputs to the SD card
-    """
-
-    def __init__(self, log_name, flush_interval=1, terminal=None):
-        """
-        Create a new instance of the class
-        :param log_name: The name to use for the log, a number preceded by a hyphen "-" will be appended to this name to avoid overwriting old logs
-        :type log_name: str
-        """
-        self.terminal = terminal
-        
-
-        self.file_name = log_directory + str(log_name) + ".log"
-        self.file_object = open(self.file_name, "w", )
-        self.write_queue = []
-        self.log("Starting log at " + self.file_name)
-
-        Thread(self.auto_flush_logs, [flush_interval])
-
-    def log(self, string: str):
-        """
-        Send a string to the file, using the log format
-        :param string:
-        """
-        try:
-            self.write_queue.append(string)
-        except MemoryError:
-            self.write_queue.clear()
-            self.log("Ran out of memory while writing logs, some messages may be omitted\n")
-
-    def exit(self):
-        """
-        Close the log object
-        """
-        self.log("Ending log at " + self.file_name)
-        self.flush_file_contents()
-        self.file_object.close()
-
-    def flush_file_contents(self):
-        try:
-            with open(self.file_name, "a") as self.file_object:
-                for _ in range(len(self.write_queue)):
-                    self.file_object.write(self.write_queue.pop(0))
-                self.file_object.flush()
-        except OSError:
-            brain.screen.print("Failed to flush log write queue (Was the SD card removed?)")
-
-    def auto_flush_logs(self, interval_sec):
-        while True:
-            wait(interval_sec, SECONDS)
-            self.flush_file_contents()
+            right_side.spin(FORWARD)
+            left_side.spin(FORWARD)
+            carter = controller_1.axis4.position() / 2 
+            carter_2 = controller_1.axis4.position() / 2
+            right_side.set_velocity((controller_1.axis3.position() - carter), PERCENT)
+            left_side.set_velocity((controller_1.axis3.position() + carter_2), PERCENT)
 
 # create competition instance
-comp = Competition(autonomous, user_control)
+comp = Competition(user_control, autonomous)
 pre_autonomous()
 # Main Controller loop to set motors to controller axis positions
 
